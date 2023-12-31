f3d39b47d1b95268d5893cfa1413d3d8da42f0bc
shounaksarker <shounaksarker@gmail.com>
2023-12-31 15:45:35 +0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <script>
-import Table from './components/Table.vue';
+import Table2 from './components/Table2.vue';
 
 export default {
+    mounted(){
+        console.log("mounted : ", new Date().toLocaleTimeString());
+    },
+    created(){
+        console.log("created : ", new Date().toLocaleTimeString());
+    },
     computed: {
         bookList() {
             // console.log(`hello, ${this.$store.state.bookList}`);
             return;
-        }
+        },
     },
-    components: { Table }
+    components: { Table2 }
 }
 </script>
 
 <template>
-  <Table/>
+  <Table2/>
 </template>
 
